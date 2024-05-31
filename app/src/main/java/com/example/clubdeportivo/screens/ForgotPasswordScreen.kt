b5dd0b771546dc5c010ec763ee8d34764bfdc3f7
Tiago <tiago.appdev@gmail..com>
2024-05-31 11:38:28 -0300

    forgot password screen added

--- app/src/main/java/com/example/clubdeportivo/screens/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/clubdeportivo/screens/ForgotPasswordScreen.kt
@@ -1,28 +1,146 @@
 package com.example.clubdeportivo.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.clubdeportivo.R
 import com.example.clubdeportivo.navigation.AppScreens
 
 @Composable
 fun ForgotPasswordScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Pantalla Olvidaste tu contraseña?")
-        Button(onClick = {
-            navController.navigate(AppScreens.LoginScreen.route)
-        }) {
-            Text(text = "Atras!")
+
+    Surface{
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+                .background(color = MaterialTheme.colorScheme.background),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(60.dp))
+            Image(
+                painter = painterResource(id = R.drawable.logo_init),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Spacer(modifier = Modifier.height(80.dp))
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.Start
+            ) {
+
+                EmailUserField(
+                    value = "",
+                    onChange = {},
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+            Spacer(modifier = Modifier.height(80.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Top,
+            ) {
+                Button(onClick = {},
+                    shape = RoundedCornerShape(5.dp),
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.onSurface,
+                        contentColor = MaterialTheme.colorScheme.surface
+                    )
+                ) {
+                    Text(
+                        fontWeight = MaterialTheme.typography.titleMedium.fontWeight,
+                        style = MaterialTheme.typography.titleMedium,
+                        text = "Restablecer Contraseña")
+                }
+            }
+
         }
     }
+}
+
+@Composable
+fun EmailUserField(
+    value: String,
+    onChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String = "Ingresa tu email"
+){
+    val focusManager = LocalFocusManager.current
+    val leadingIcon = @Composable {
+        Icon(
+            Icons.Default.Email,
+            contentDescription = "",
+            tint = Color(0xFFF14D56)
+        )
+    }
+    Text(
+        color = MaterialTheme.colorScheme.onBackground,
+        text = "Email de cuenta registrada",
+        modifier = Modifier.padding(start = 10.dp),
+        fontWeight = FontWeight.Bold,
+        fontSize = MaterialTheme.typography.titleMedium.fontSize)
+
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        modifier = modifier,
+        leadingIcon = leadingIcon,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(
+            onNext = {focusManager.moveFocus(FocusDirection.Down)}
+        ),
+        placeholder = {
+            Text(
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                text = placeholder) },
+        singleLine = true,
+        visualTransformation = VisualTransformation.None,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surfaceDim,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface
+        )
+    )
 }
