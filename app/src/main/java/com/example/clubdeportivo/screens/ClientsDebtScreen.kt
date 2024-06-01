6bcfc224474eff1f8c1a0774f9c4ffd70b369b53
Dario Skidelsky <76440563+darioque@users.noreply.github.com>
2024-06-01 20:25:01 -0300

    adds stylizing

--- app/src/main/java/com/example/clubdeportivo/screens/ClientsDebtScreen.kt
+++ app/src/main/java/com/example/clubdeportivo/screens/ClientsDebtScreen.kt
@@ -1,7 +1,8 @@
 package com.example.clubdeportivo.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,7 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -30,9 +31,10 @@ import com.example.clubdeportivo.R
 fun ClientsDebt(navController: NavController) {
     // Dummy data for demonstration
     val debtors = listOf(
-        Debtor("Juan Perez", "123456", "2024-05-01", 500),
-        Debtor("Javier Lopez", "789012", "2024-05-10", 750),
-        Debtor("Alicia Rodriguez", "345678", "2024-04-20", 300)
+        Debtor("Juan Perez", "123456", "2024/05/01", 500),
+        Debtor("Javier Lopez", "789012", "2024/05/10", 750),
+        Debtor("Alicia Rodriguez", "345678", "2024/04/20", 300),
+        Debtor("Luciana Gonzalez", "316712", "2024/01/02", 1200),
     )
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -63,21 +65,33 @@ fun ClientsDebt(navController: NavController) {
 @Composable
 fun DebtorItem(debtor: Debtor) {
     Surface(
+        color = Color.LightGray, // Set the background color to gray
         shape = RoundedCornerShape(16.dp),
-        modifier = Modifier.fillMaxWidth().shadow(4.dp)
+        modifier = Modifier.fillMaxWidth().shadow(12.dp)
     ) {
         Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .background(MaterialTheme.colorScheme.surface)
+            modifier = Modifier.padding(16.dp)
         ) {
-            Text(text = debtor.name)
-            Text(text = "Número de cliente: ${debtor.clientNumber}", textAlign = TextAlign.End)
-            Text(text = "Fecha de inicio de la deuda: ${debtor.startDate}", textAlign = TextAlign.End)
-            Text(text = "Monto adeudado: $${debtor.amount}", textAlign = TextAlign.End)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = debtor.name, fontWeight = FontWeight.Bold)
+                Text(
+                    text = "$${debtor.amount}",
+                    textAlign = TextAlign.End,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Text(text = "Número de cliente: ${debtor.clientNumber}", textAlign = TextAlign.End,
+                fontWeight = FontWeight.Light)
+            Text(text = "Desde el ${debtor.startDate}", textAlign = TextAlign.End,
+                fontWeight = FontWeight.Light)
         }
     }
 }
+
+
 
 data class Debtor(
     val name: String,
